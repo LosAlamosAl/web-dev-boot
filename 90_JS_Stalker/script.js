8b407c44d5ac81a50c26987b163efebddb6eecf4
Allen L. McPherson <mcpherson@lanl.gov>
2018-01-13 18:27:04 -0700

    Adding new exercise

---
+++ 90_JS_Stalker/script.js
@@ -0,0 +1,5 @@
+var firstName = prompt("First name?");
+var lastName = prompt("Last name?");
+var age = prompt("Age?");
+console.log("I am " + firstName + " " + lastName);
+console.log("He is " + age + " years old");
