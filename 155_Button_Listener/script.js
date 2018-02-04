eb9a78b9956a130c70ccb86144f81ac5d2803559
Los Alamos Al <losalamosal@allenmcpherson.com>
2018-02-04 15:45:57 -0700

    Added button event listener

---
+++ 155_Button_Listener/script.js
@@ -0,0 +1,6 @@
+b = document.getElementsByTagName("button")[0];
+b.addEventListener("click", function() {
+  bod = document.getElementsByTagName("body")[0];
+  bod.style.background = "green";
+});
+
