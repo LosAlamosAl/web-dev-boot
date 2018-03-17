b219f22c05fcaecfaafac3996ee71771b2985c4f
Los Alamos Al <losalamosal@allenmcpherson.com>
2018-03-16 18:10:45 -0600

    Updating for vanilla JavaScript

--- 187_To_Do/script.js
+++ 187_To_Do/script.js
@@ -1,27 +1,75 @@
 //  Click on to-do to toggle done/undone
-$("ul").on("click", "li", function(){
-  $(this).toggleClass("done");
+const lis = document.getElementsByClassName('ltodo');
+[].forEach.call(lis, function(elem) {
+  elem.addEventListener('click', function (event) {
+    this.classList.toggle('done');
+  });
 });
+
+//  Working JQuery version of above...
+//$("ul").on("click", "li", function(){
+//  $(this).toggleClass("done");
+//});
+
+
+
+
 
 //  Click on the garbage can (the spans in the list) to delete to-do
-$("ul").on("click", "span", function(event){
-  $(this).parent().fadeOut(500,function(){
-    $(this).remove();
+const spans = document.querySelectorAll('ul span');
+[].forEach.call(spans, function(elem) {
+  elem.addEventListener('click', function (event) {
+    //  Maybe the fade can be done with add/remove CSS class? (@keyframes)
+    var s = this.parentNode.style;
+    s.opacity = 1;
+    (function fade() {
+      (s.opacity -= .1) < .1 ? s.display="none" : setTimeout(fade, 30);
+    })();
+    event.stopPropagation();
+    //this.parentNode.remove();
   });
-  // need to do this or event bubbles up
-  event.stopPropagation();
 });
 
+//  Working JQuery version of above...
+//$("ul").on("click", "span", function(event){
+//  $(this).parent().fadeOut(500,function(){
+//    $(this).remove();
+//  });
+//  // need to do this or event bubbles up
+//  event.stopPropagation();
+//});
+
+
+
+
+
 //  Watch keystrokes in input area. Upon newline (return) add to-do
-$("input[type='text']").keypress(function(event){
-  if(event.which === 13){
-    //grabbing new todo text from input
-    var todoText = $(this).val();
-    $(this).val("");
-    //create a new li and add to ul
-    $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>");
-  }
-});
+
+
+
+
+
+
+
+
+
+
+
+//  Working JQuery version of above...
+// $("input[type='text']").keypress(function(event){
+//   if(event.which === 13){
+//     var todoText = $(this).val();
+//     if ($(this).val() !== "") {
+//       $(this).val("");
+//       $("ul").append("<li class='ltodo'><span><i class='fa fa-trash-alt'></i></span> " + todoText + "</li>");
+//     }
+//   }
+// });
+
+
+
+
+
 
 //  Add or remove the text area when big plus sign is clicked
 $("#plus").click(function(){
